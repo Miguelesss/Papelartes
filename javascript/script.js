f7dd24f493e2680ca092b20269e15d5ba4ab6e0c
Miguel Angelo <134246672+Miguelesss@users.noreply.github.com>
2024-01-07 18:51:41 -0300

    menu de opções

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+
+function opMenu(){
+    let open = document.getElementById("menu-mb");
+    open.style.display = "inline-block";
+    open.style.transform = "none";
+}
+
+function clMenu(){
+    let close = document.getElementById("menu-mb");
+    close.style.transform = "translateX(100%)";
+    close.style.animationName = "slideout"
+}
+
+function hover(){
+
+}
+
+
+
